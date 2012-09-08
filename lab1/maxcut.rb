112f5b0ab43bb83419c0d8beccb07871adec959d
Oscar Olsson <osse.olsson@gmail.com>
2012-09-08 18:32:58 +0200

    Fixed between method

--- lab1/maxcut.rb
+++ lab1/maxcut.rb
@@ -9,7 +9,7 @@ class Edge
 	end
 
 	def between?(set1, set2)
-		set1.include? @v1 and not set2.include? @v2 or not set1.include? @v1 and set2.include? @v2
+		(set1.include? @v1 and set2.include? @v2) or (set2.include? @v1 and set1.include? @v2)
 	end
 end
 
@@ -18,8 +18,6 @@ edges = Set.new
 File.foreach(ARGV[0]){ |line|
 	values = line.split(' ')
 	if values.size == 3
-		# v1 = Vertex.new(values[0])
-		# v2 = Vertex.new(values[1])
 		v1 = values[0].to_i
 		v2 = values[1].to_i
 		vertices.add(v1)
@@ -29,7 +27,7 @@ File.foreach(ARGV[0]){ |line|
 }
 results = []
 100.times do
-	# Construct the set of selected vertecies
+	# Construct the set of selected verticies
 	v_a = Set.new
 	vertices.each{ |v|
 		# Select by randomly decide for each vertex
@@ -37,9 +35,9 @@ results = []
 			v_a.add(v)
 		end
 	}
-	# Construct a set of the vertecies not selected
+	# Construct a set of the verticies not selected
 	v_b = vertices - v_a
-	# Contruct the set of edges cosituting the cut
+	# Contruct the set of edges constituting the cut
 	cut = Set.new
 	edges.each { |e|
 		if e.between?(v_a, v_b)
@@ -54,3 +52,5 @@ results = []
 	results.push(total_weight)
 end
 puts results
+avg = results.inject { |sum, res| sum += res } / results.size
+puts "Average: #{avg}"
