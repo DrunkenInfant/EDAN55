ea133dbc7dbbf19292a1b341e9ca44eebc15c5e9
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2012-09-14 12:25:08 +0200

    vi har ett fel i kööden

--- lab2/src/indset.rb
+++ lab2/src/indset.rb
@@ -2,13 +2,37 @@
 class Vertex
 	attr_accessor :id, :neighbours
 
-	def initialize(id, neighbours)
+	def initialize(id)
 		@id = id
-		@neighbours = neighbours
+		@neighbours = Set.new
 	end
 
 	def neighbourhood()
-		@neighbours + Set.new(@iid)
+		@neighbours + Set.new(@id)
+	end
+
+	def add_neighbour(id)
+		@neighbours.add(id)
 	end
 
 end
+
+vertices = Set.new
+id = 0
+
+# Read input
+File.foreach(ARGV[0]) { |line|
+	values = line.split(" ")
+	if values.size != 1
+		v = Vertex.new(id)
+		values.each_with_index do |value,index|
+			if value == 1
+				v.add_neighbour(index)
+			end
+		end
+		vertices.add(v)
+		id++
+	end
+}
+
+puts "hej"
