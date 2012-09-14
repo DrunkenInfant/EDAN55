04efa1b804e813b2159d9dcd8067f8dec7232ab6
Oscar Olsson <osse.olsson@gmail.com>
2012-09-14 11:47:43 +0200

    Added vertex class

--- lab2/src/indset.rb
+++ lab2/src/indset.rb
@@ -1,2 +1,14 @@
 
-puts 'Independent Set!'
+class Vertex
+	attr_accessor :id, :neighbours
+
+	def initialize(id, neighbours)
+		@id = id
+		@neighbours = neighbours
+	end
+
+	def neighbourhood()
+		@neighbours + Set.new(@iid)
+	end
+
+end
